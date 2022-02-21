b8f64d41c65d3c849e4cf416a53fc27df7cfb5a6
Xihai Luo <xihai.luo01@gmail.com>
2022-02-21 11:12:31 -0500

    update parseSentence to handle more complex sentences

--- test/test.js
+++ test/test.js
@@ -5,28 +5,42 @@ const { parseSentence } = require("../parseSentence");
 const { compareSentence } = require("../compareSentence");
 const { sort } = require("../sort");
 
+// easy story to test
 const path = "./test/sample1.txt";
+// complex story to test
+const path2 = "./test/sample2.txt";
+
 const mock = {
   story: {
     1: [
-      "The primary purpose of assessment and evaluation is to improve student learning.",
-      "\nInformation gathered through assessment helps teachers to determine students’ strengths and weaknesses in their achievement of the curriculum expectations.",
-      "\nEvaluation refers to the process of judging the quality of student work on the basis of established criteria, and assigning a value to represent that quality.",
-      "\nAssessments and evaluations are varied in nature, administered over a period of time, and designed to provide opportunities for students to demonstrate the full range of their learning.",
-      "\nAll assessments and evaluations will be related to the learning activities used, correspond to the purposes of instruction, match the needs and experiences of the students, and be fair to all students.",
-      "\nAs they are enrolled in a University Preparation level course, students of MCR3U can expect a majority of evaluations to be in test form.",
-      "\nTest in this instance refers to evaluative tools designed to assess the pupils' ability to think mathematically and to explain their thinking, with an emphasis on 'explaining why', rather than just 'knowing how'.",
+      "\rThe primary purpose of assessment and evaluation is to improve student learning.",
+      "\rInformation gathered through assessment helps teachers to determine students’ strengths and weaknesses in their achievement of the curriculum expectations.",
+      "\rEvaluation refers to the process of judging the quality of student work on the basis of established criteria, and assigning a value to represent that quality.",
+      "\rAssessments and evaluations are varied in nature, administered over a period of time, and designed to provide opportunities for students to demonstrate the full range of their learning.",
+      "\rAll assessments and evaluations will be related to the learning activities used, correspond to the purposes of instruction, match the needs and experiences of the students, and be fair to all students.",
+      "\rAs they are enrolled in a University Preparation level course, students of MCR3U can expect a majority of evaluations to be in test form.",
+      "\rTest in this instance refers to evaluative tools designed to assess the pupils' ability to think mathematically and to explain their thinking, with an emphasis on 'explaining why', rather than just 'knowing how'.",
+    ],
+    2: [
+      '\rThe stars and Galaxies died and snuffed out, and space grew black after ten trillion years of running down.',
+      "\rOne by one Man fused with AC, each physical body losing its mental identity in a manner that was somehow not a loss but a gain.",
+      "\rMan's last mind paused before fusion, looking over a space that included nothing but the dregs of one last dark star and nothing besides but incredibly thin matter, agitated randomly by the tag ends of heat wearing out, asymptotically, to the absolute zero.",
+      '\rMan said, AC, is this the end?',
+      "\rCan this chaos not be reversed into the Universe once more?",
+      "\rCan that not be done?",
+      `\rAC said, THERE IS AS YET INSUFFICIENT DATA FOR A MEANINGFUL ANSWER.`,
+      `\rMan's last mind fused and only AC existed -- and that in hyperspace.`,
     ],
   },
   sortedStory: {
     1: [
-      "\nAll assessments and evaluations will be related to the learning activities used, correspond to the purposes of instruction, match the needs and experiences of the students, and be fair to all students.",
-      "\nAssessments and evaluations are varied in nature, administered over a period of time, and designed to provide opportunities for students to demonstrate the full range of their learning.",
-      "\nAs they are enrolled in a University Preparation level course, students of MCR3U can expect a majority of evaluations to be in test form.",
-      "\nEvaluation refers to the process of judging the quality of student work on the basis of established criteria, and assigning a value to represent that quality.",
-      "\nInformation gathered through assessment helps teachers to determine students’ strengths and weaknesses in their achievement of the curriculum expectations.",
-      "\nTest in this instance refers to evaluative tools designed to assess the pupils' ability to think mathematically and to explain their thinking, with an emphasis on 'explaining why', rather than just 'knowing how'.",
-      "The primary purpose of assessment and evaluation is to improve student learning.",
+      "\rAll assessments and evaluations will be related to the learning activities used, correspond to the purposes of instruction, match the needs and experiences of the students, and be fair to all students.",
+      "\rAssessments and evaluations are varied in nature, administered over a period of time, and designed to provide opportunities for students to demonstrate the full range of their learning.",
+      "\rAs they are enrolled in a University Preparation level course, students of MCR3U can expect a majority of evaluations to be in test form.",
+      "\rEvaluation refers to the process of judging the quality of student work on the basis of established criteria, and assigning a value to represent that quality.",
+      "\rInformation gathered through assessment helps teachers to determine students’ strengths and weaknesses in their achievement of the curriculum expectations.",
+      "\rTest in this instance refers to evaluative tools designed to assess the pupils' ability to think mathematically and to explain their thinking, with an emphasis on 'explaining why', rather than just 'knowing how'.",
+      "\rThe primary purpose of assessment and evaluation is to improve student learning.",
     ],
   },
 };
@@ -58,6 +72,23 @@ describe("array with sentences", () => {
         const result = parseSentence(data);
         assert.isArray(result);
         assert.deepEqual(mock.story[1], result);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it("should parse complex sentences", (done) => {
+    // dut === parser
+    // input === data
+    // output === array of sentences
+    getStoryData(path2)
+      .then((data) => {
+        assert.isOk("Data successfully loaded");
+        const result = parseSentence(data);
+        assert.isArray(result);
+        assert.deepEqual(mock.story[2], result);
         done();
       })
       .catch((err) => {
